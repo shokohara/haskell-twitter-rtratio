1733d4117b2029055eb47f26bdf95013d4e505a2
shokohara <shokohara@users.noreply.github.com>
2016-12-27 13:38:18 +0900

    WIP

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,41 @@
 module Main where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
-import Lib
+import Web.Authenticate.OAuth
+import Data.Text (Text)
+import qualified Data.Text.IO as T
+import Data.Aeson
+import GHC.Generics
+import Network.HTTP.Conduit
+
+myOAuth :: OAuth
+myOAuth = newOAuth { oauthServerName = "api.twitter.com"
+                   , oauthConsumerKey = "Your Consumer Key"
+                   , oauthConsumerSecret = "Your Cosumer Secret"
+                   }
+
+myCred :: Credential
+myCred = newCredential "Your Access Token" "Your Access Token Secret"
+
+data Tweet = Tweet { text :: Text } deriving (Show, Generic)
+
+instance FromJSON Tweet
+instance ToJSON Tweet
+
+getTweets :: String -> IO (Either String [Tweet])
+getTweets name = do
+    req <- parseUrl $ "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" ++ name
+    m <- newManager tlsManagerSettings
+    res <- do
+            signedreq <- signOAuth myOAuth myCred req
+            httpLbs signedreq m
+    return $ eitherDecode $ responseBody res
 
 main :: IO ()
-main = someFunc
+main = do
+    ets <- getTweets "ishiy1993"
+    case ets of
+      Left err -> putStrLn err
+      Right ts -> mapM_ T.putStrLn . map text $ take 5 ts
+
